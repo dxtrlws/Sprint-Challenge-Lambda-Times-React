05c603e22b297dfb220379a375809b272cbc06fe
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-24 21:50:44 -0400

    Add proptype validation

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import Tab from "./Tab";
 const Tabs = props => {
   return (
@@ -16,4 +17,10 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+  changeSelected: PropTypes.func
+};
+
 export default Tabs;
